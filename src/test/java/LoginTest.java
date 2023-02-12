8081f58e2b6d5e6c47d73afc9acb71bf8b34cde3
Gabriel santana <gabrielssilvast@gmail.com>
2023-02-12 20:22:06 -0300

    first commit

---
+++ src/test/java/LoginTest.java
@@ -0,0 +1,46 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class LoginTest {
+    WebDriver driver = new ChromeDriver();
+
+    @BeforeEach
+    void setUp() {
+        System.setProperty("WebDriver.chrome.driver", "drivers/chromedriver.exe");
+        driver.get("https://automationexercise.com/login");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().setSize(new Dimension(1000, 1000));
+    }
+
+//    @AfterEach
+//    void tearDown() {
+//        driver.quit();
+//    }
+
+    @Test
+    void test() {
+        WebElement emailAddressLocator = driver.findElement(By.name("email"));
+//        new WebDriverWait(driver, Duration.ofSeconds(5));
+        emailAddressLocator.sendKeys("gabrielssilvast@gmail.com");
+
+        WebElement passwordElement = driver.findElement(By.xpath("//*[@id=\"form\"]/div/div/div[1]/div/form/input[3]"));
+        passwordElement.sendKeys("123456");
+
+        WebElement loginLocator = driver.findElement(By.xpath("//*[@id=\"form\"]/div/div/div[1]/div/form/button"));
+        loginLocator.click();
+
+//        WebElement tagLogout = driver.findElement(By.xpath("//*[@id=\"header\"]/div/div/div/div[2]/div/ul/li[4]/a"));
+//        String ConfirmLogin = tagLogout.getText();
+//
+//        Assertions.assertTrue(ConfirmLogin.equals("Logout"));
+    }
+
+    }
